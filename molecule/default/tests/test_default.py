9ec6d7825906650244034573ce1456c17ee9adc8
Zakhar Kleyman <zakhar.kleyman@mongodb.com>
2020-06-09 11:40:36 -0400

    feat(BUILD-11337): add ruby toolchain (#1)
    * feat(BUILD-11337): add ruby toolchain
    
    * Remove unused vars

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,5 +7,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_host(host):
-    assert host.file("/etc/hosts").exists
+def test_ruby_toolchain(host):
+    revision = "a650fe6064a65922754444f3fe04c248cdf50ef1"
+    assert host.file("/opt/ruby").exists
+    assert host.file("/opt/ruby/toolchain_version").exists
+    assert host.file("/opt/ruby/toolchain_version").contains(revision)
+
+
+def test_toolchain_version(host):
+    revision = "a650fe6064a65922754444f3fe04c248cdf50ef1"
+    assert host.file("/opt/ruby/toolchain_version").exists
+    assert host.file("/opt/ruby/toolchain_version").contains(revision)
+
+
+def test_rubies_symlink(host):
+    assert host.file("/root/.rubies").is_symlink
+    assert host.file("/root/.rubies").linked_to == "/opt/ruby"
